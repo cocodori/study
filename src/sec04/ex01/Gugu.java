e24684784dc294eeabcb35d642e1192a0f68f52e
cocodori <moradulee@gmail.com>
2020-08-05 23:37:27 +0900

    구구단 예제

---
+++ src/sec04/ex01/Gugu.java
@@ -0,0 +1,58 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Gugu
+ */
+@WebServlet("/gugu")
+public class Gugu extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		int dan = Integer.parseInt(request.getParameter("dan"));
+		
+		System.out.println("dan : " + dan);
+		
+		String data = "";
+		
+		data += "<table border=1 width=800 align=center>";
+		data += "<tr align=center bgcolor='#FFFF66'>";
+		data += "<td colspan=2" + dan + "단 출력 </td>";
+		data += "</tr>";
+		
+		for(int i=1; i<10; i++) {
+			if(i % 2 == 0 ) {	//곱하는 수가 2의 배수면 초록색
+				data += "<tr align=center bgcolor='#ACFA58'>";
+			} else { //아니면 파란색
+				data += "<tr align=center bgcolor='#81BEF7'>";
+			}
+			data += "<td width=200>";
+			data += "<input type='radio' />" + i;
+			data += "</td>";
+			data += "<td width=200>";
+			data += "<input type='checkbox' />" +i;
+			data += "</td>";
+			data += "<td width=400>";
+			data += dan + " * " + i;
+			data += "</td>";
+			data += "<td width=400>";
+			data += i*dan;
+			data += "</td></tr>";
+		}
+		
+		data += "</table>";
+		out.print(data);
+	}
+
+
+}
---
+++ src/sec04/ex01/LoginTest2.java
@@ -0,0 +1,60 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginTest2
+ */
+@WebServlet("/loginTest2")
+public class LoginTest2 extends HttpServlet {
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("hello...init()...");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("username");
+		String pw = request.getParameter("password");
+		
+		System.out.println(" ID : " + id);
+		System.out.println(" PW : " + pw);
+		
+		String data = "";
+		
+		if(id!=null&&(id.length()!=0)) {
+			if(id.equals("admin")) {
+				data += "<html><body>";
+				data += "<h1> 관리자 계정으로 로그인 하셨습니다.</h1>";
+				data += "<input type='button' value='회원정보 등록'/>";
+				data += "<input type='button' value='회원정보 수정'/>";
+				data += "<input type='button' value='회원정보 삭제'/>";
+				data += "</body></html>";
+				
+				out.print(data);
+			} else {
+				data += "<html><body>";
+				data += "<h1>"+id+"님이 로그인 하셨습니다.</h1>";
+				data += "</body></html>";
+				
+				out.print(data);
+			}
+		} else {
+			data += "<html><body>";
+			data += "<h1>아이디를입력하세요.</h1> <br> <a href='http://localhost:8080/test01/login.html'>돌아가기</a>";
+			data += "</body></html>";
+			
+			out.print(data);
+		}
+	}
+
+}
