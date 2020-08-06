2c8979f3f17ee9ec212a98a17507899417267650
cocodori <moradulee@gmail.com>
2020-08-06 15:49:28 +0900

    JDBC -> DataSource 바꾸기

--- src/pro07/sec01/ex01/MemberDAO.java
+++ src/pro07/sec01/ex01/MemberDAO.java
@@ -1,7 +1,6 @@
 package pro07.sec01.ex01;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,20 +8,45 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class MemberDAO {
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";	//DRIVER NAME
-	private static final String URL = "jdbc:mysql://127.0.0.1:3306/servletex?serverTimezone=Asia/Seoul";	//JDBC URL/스키마
-	private static final String USER = "servlet"; //DB ID
-	private static final String PWD = "1234";	  //DB PW
+	/* JDBC 설정
+	 * private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; //DRIVER
+	 * NAME private static final String URL =
+	 * "jdbc:mysql://127.0.0.1:3306/servletex?serverTimezone=Asia/Seoul"; //JDBC
+	 * URL/스키마 private static final String USER = "servlet"; //DB ID private static
+	 * final String PWD = "1234"; //DB PW
+	 */	
 	
 	private Connection con;
 	private PreparedStatement pstmt;
+	//DataSource설정
+	private DataSource ds;
+	
+	public MemberDAO() {
+		try {
+			//JNDI에 접근하기 위해 기본 경로를 지정
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			//context.xml에 설정한 name값을 이용해 톰캣이 미리 연결한 DataSource를 받아온다.
+			ds = (DataSource) envContext.lookup("jdbc/mysql");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} //생성자
 	
 	List<MemberVO> listMembers() {
 		List<MemberVO> list = new ArrayList<>();
 
 		try {
-			connectDB();
+//			connectDB(); //JDBC설정
+			
+			//DataSource를 이용해 데이터베이스에 연결한다.
+			con = ds.getConnection();
 			String sql = "select * from t_member";
 			System.out.println("Query : " + sql);
 			pstmt = con.prepareStatement(sql);
@@ -65,16 +89,13 @@ public class MemberDAO {
 		return list;
 	} //listMembers()
 	
-	private void connectDB() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("DRIVER LOADING.....");
-			con = DriverManager.getConnection(URL, USER, PWD);
-			System.out.println("Connection 생성");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	/* JDBC설정
+	 * private void connectDB() { try { Class.forName("com.mysql.cj.jdbc.Driver");
+	 * System.out.println("DRIVER LOADING....."); con =
+	 * DriverManager.getConnection(URL, USER, PWD);
+	 * System.out.println("Connection 생성");
+	 * 
+	 * } catch (Exception e) { e.printStackTrace(); } }
+	 */
 	
 }
