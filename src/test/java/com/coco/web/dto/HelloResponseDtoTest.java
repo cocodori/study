e4c80575aa90e446e8290d0ab841643bc8eecf3f
cocodori <moradulee@gmail.com>
2020-09-14 16:31:35 +0900

    lombok추가 + 테스트

---
+++ src/test/java/com/coco/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.coco.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest() {
+        //given
+        String name = "test";
+        int amount = 100;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
