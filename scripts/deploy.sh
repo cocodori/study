7696bdda65394bd258f0b6d713dbb29116d746bd
cocodori <moradulee@gmail.com>
2020-09-23 17:09:55 +0900

    배포 자동 재설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
 
-CURRENT_PID=$(pgrep fl bootex | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl bootex | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 PID : $CURRENT_PID"
 
@@ -23,17 +23,17 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
 
 echo "> JAR_NAME 에 실행 권한 추가"
 
-chmod +x $REPOSITORY/*.jar
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
--Dspring.config.location=classpath:/applcation.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
--Dspring.profiles.active=real \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=real \
  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
