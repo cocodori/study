b438d979e1abc89722621db4183ef443e758f7ce
cocodori <moradulee@gmail.com>
2020-10-03 22:51:46 +0900

    레이아웃 적용

---
+++ src/main/java/com/coco/board/controller/BoardController.java
@@ -0,0 +1,45 @@
+package com.coco.board.controller;
+
+import com.coco.board.dto.BoardDTO;
+import com.coco.board.dto.PageRequestDTO;
+import com.coco.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Log4j2
+@RequestMapping("/board/*")
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("----------list-----------");
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("------------register-------------");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO boardDto, RedirectAttributes redirect) {
+        log.info(" boardDto : " + boardDto);
+
+        //새로 추가된 엔티티 번호
+        Long bno = boardService.register(boardDto);
+
+        redirect.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
+}
