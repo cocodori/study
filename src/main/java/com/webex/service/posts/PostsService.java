983f71a71d6aec7de29fe4b09358eda7242fcddb
cocodori <moradulee@gmail.com>
2020-10-06 15:53:54 +0900

    등록, 수정 api 작성 ㅡ 테스트 완료

---
+++ src/main/java/com/webex/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.webex.service.posts;
+
+import com.webex.domain.posts.Posts;
+import com.webex.domain.posts.PostsRepository;
+import com.webex.web.dto.PostsResponseDto;
+import com.webex.web.dto.PostsSaveRequestDto;
+import com.webex.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts post = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        post.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
