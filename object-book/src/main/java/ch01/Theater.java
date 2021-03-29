aba5262cc0577b91c08dc592b2ae197266aa3b46
cocodori <moradulee@gmail.com>
2021-03-29 23:08:38 +0900

    add: [오브젝트 ch01] TicketSeller로 책임 위임

--- object-book/src/main/java/ch01/Theater.java
+++ object-book/src/main/java/ch01/Theater.java
@@ -8,21 +8,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        //관객의 가방을 열어서 초대장이 있는 지 살펴본다.
-        if(audience.getBag().hasInvitation()) {
-            //티켓 셀러가 티켓 오피스에서 티켓을 하나 꺼낸 뒤,
-            //초대장을 받은 관객의 가방을 열고 티켓을 넣어준다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            //티켓 셀러가 티켓 오피스에서 티켓을 하나 꺼낸다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            //관객의 가방을 열고 돈을 티켓 값을 빼온다.
-            audience.getBag().minusAmount(ticket.getFee());
-            //티켓셀러가 티켓오피스에 관객에게 받은 돈을 넣는다.
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            //다시 관객의 가방을 열고 티켓을 넣어준다/
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
--- object-book/src/main/java/ch01/TicketSeller.java
+++ object-book/src/main/java/ch01/TicketSeller.java
@@ -7,7 +7,21 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+//    자신이 TicketOffice를 포함하고 있다는 것을 숨겨야 한다.
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
+
+    //인터페이스
+    public void sellTo(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
